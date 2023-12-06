f6d61ece611a65d173952fb453c6000b5780b48c
Chango961204 <metallicaluis73@gmail.com>
2023-12-06 15:38:08 -0600

    agregar url backend render

--- src/app.js
+++ src/app.js
@@ -10,8 +10,16 @@ import productRoutes from './routes/products.routes.js';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials:true
+    origin: [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://localhost',
+        'http:/localhost/productos',
+        'https://apiproductos-hkzt.onrender.com',
+        'https://frontend-yu5l.onrender.com',
+
+    ],
+    credentials: true
 }));
 app.use(morgan('dev'));
 app.use(express.json());
